73b03d3febf81e963c54b646df5b754330b7be8d
Ramon <ramonesc3po@gmail.com>
2019-08-28 16:05:05 -0300

    Add more outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "ecs_service_name" {
-  value = "${element(concat(aws_ecs_service.this.*.name, list("")),0)}"
+  value = element(concat(aws_ecs_service.this.*.name, list("")),0)
+}
+
+output "ecs_task_family_name" {
+  value = element(concat(aws_ecs_task_definition.this.*.family, list(""), 0))
 }

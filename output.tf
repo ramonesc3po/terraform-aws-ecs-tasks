5a4f07dbde447e1130bfa7ef81ce2cc8079178b3
Ramon <ramonesc3po@gmail.com>
2019-09-16 11:25:21 -0300

    Add name_ecs_task #2

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "ecs_task_family_name" {
   value = element(concat(aws_ecs_task_definition.this.*.family, list("")), 0)
 }
 
-output "ecs_task_name" {
+output "name_ecs_task" {
   value = local.name_ecs_task
 }
